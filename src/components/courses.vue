6d0c0c8ca26206ae110b028d0e6d14d8c23349f1
Tong <temberature@gmail.com>
2017-12-19 14:05:01 +0800

    vue-infinite-scroll

---
+++ src/components/courses.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="app"
+         style="height: 1200px;background-color: #ccc;width: 400px;margin: 0 auto;"
+         v-infinite-scroll="loadMore"
+         infinite-scroll-disabled="busy"
+         infinite-scroll-distance="10">
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'courses',
+    data () {
+      return {busy: false}
+    },
+    methods: {
+      loadMore: function () {
+        var self = this
+        self.busy = true
+        console.log('loading... ' + new Date())
+        setTimeout(function () {
+          var app = document.querySelector('.app')
+          var height = app.clientHeight
+          app.style.height = height + 300 + 'px'
+          console.log('end... ' + new Date())
+          self.busy = false
+        }, 1000)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    a {
+        color: #42b983;
+    }
+</style>
